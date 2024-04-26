a13c33070ecdc1718fc3c9ef72ba1bd6e11bab6e
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-04-26 13:08:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button = document.querySelector('input')
+let select = document.getElementById('colorSelect')
+
+button.addEventListener("click",()=>{
+	select.remove(select.selectedIndex)
+})
